fc44c6adf049881c718fe5bedcecfdc394f9e3a9
Sabbir Rahman Sakib <sabbir.sakib@cepheid.com>
2019-03-22 18:23:43 +0100

    create friend table migration

---
+++ bin/main/db/migration/1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE friend (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  first_name varchar(255) DEFAULT NULL,
+  last_name varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
