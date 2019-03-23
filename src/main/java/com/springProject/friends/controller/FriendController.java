02cacb7265c4f85683176ed415906568051719ff
Sabbir Rahman Sakib <sabbir.sakib@cepheid.com>
2019-03-23 21:15:08 +0100

    find by id , first and last

--- src/main/java/com/springProject/friends/controller/FriendController.java
+++ src/main/java/com/springProject/friends/controller/FriendController.java
@@ -1,5 +1,7 @@
 package com.springProject.friends.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springProject.friends.model.Friend;
@@ -36,5 +39,25 @@ public class FriendController {
 	@DeleteMapping("friend/{id}")
 	void delete(@PathVariable Integer id) {
 		friendService.deleteById(id);
+	}
+	
+	@GetMapping("friend/{id}")
+	Optional<Friend> findById(@PathVariable Integer id) {
+		return friendService.findById(id);
+	}
+	
+	@GetMapping("friend/search")
+	Iterable<Friend> findByQuery(
+			@RequestParam(value = "first", required = false) String firstname, 
+			@RequestParam(value = "last", required = false) String lastname) {
+		if(firstname != null && lastname != null)
+		return friendService.findByFirstNameAndLastName(firstname, lastname);
+		if(firstname != null)
+			return friendService.findByFirstName(firstname);
+		if(lastname != null)
+			return friendService.findByLastName(lastname);
+		return
+			friendService.findAll();
+		
 	}
 }
--- src/main/java/com/springProject/friends/service/FriendService.java
+++ src/main/java/com/springProject/friends/service/FriendService.java
@@ -1,8 +1,13 @@
 package com.springProject.friends.service;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import com.springProject.friends.model.Friend;
 
 public interface FriendService extends CrudRepository<Friend, Integer> {
 
+	Iterable<Friend> findByFirstNameAndLastName(String firstname, String lastname);
+	Iterable<Friend> findByFirstName(String firstname);
+	Iterable<Friend> findByLastName(String lastname);
 }
