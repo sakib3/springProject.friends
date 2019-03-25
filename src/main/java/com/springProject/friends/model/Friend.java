cb2521af0657af29f3b36dcebc881b381039dc17
Sabbir Rahman Sakib <sabbir.sakib@cepheid.com>
2019-03-25 17:59:33 +0100

    one to many: friend has many addresses

--- src/main/java/com/springProject/friends/model/Friend.java
+++ src/main/java/com/springProject/friends/model/Friend.java
@@ -1,17 +1,31 @@
 package com.springProject.friends.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class Friend {
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@JsonProperty("first-name")
 	private String firstName;
+	@JsonProperty("last-name")
 	private String lastName;
+	private int age;
+	@JsonIgnore
+	private boolean married;
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Address> addresses;
 
 	public String getFirstName() {
 		return firstName;
@@ -36,4 +50,29 @@ public class Friend {
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isMarried() {
+		return married;
+	}
+
+	public void setMarried(boolean married) {
+		this.married = married;
+	}
+
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+
 }
