5a5316a44bf805f1717232cee38091ac1ec826c0
Sabbir Rahman Sakib <sabbir.sakib@cepheid.com>
2019-03-26 16:42:35 +0100

    Smoke and Sanity Test

--- src/test/java/com/springProject/friends/FriendsApplicationTests.java
+++ src/test/java/com/springProject/friends/FriendsApplicationTests.java
@@ -1,16 +1,23 @@
 package com.springProject.friends;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springProject.friends.controller.FriendController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FriendsApplicationTests {
+	@Autowired
+	FriendController friendController;
 
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(friendController);
 	}
 
 }
